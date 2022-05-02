043f12b38f60e2f5d5f04f51edc6158bf253d7da
Alexandra <al88.korotkevich@gmail.com>
2022-05-03 00:56:43 +0300

    fix: fix remove from list

--- src/remove-from-list.js
+++ src/remove-from-list.js
@@ -1,4 +1,4 @@
-// const { ListNode } = require('../extensions/list-node.js');
+const { ListNode } = require('../extensions/list-node.js');
 
 /**
  * Given a singly linked list of integers l and an integer k,
@@ -21,7 +21,7 @@
  * }
  */
 function removeKFromList(l, k) {
-  let list = l;
+  let list = new ListNode();
   list.next = l;
   let prev = list;
   let curr= l;
